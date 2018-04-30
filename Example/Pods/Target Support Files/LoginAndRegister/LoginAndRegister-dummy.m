b351d0c89e499802963f95687124e90d5266f164
Elaine <elaine_zhao@acmedcare.com>
2018-04-30 11:12:02 +0800

    登录

---
+++ Example/Pods/Target Support Files/LoginAndRegister/LoginAndRegister-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LoginAndRegister : NSObject
+@end
+@implementation PodsDummy_LoginAndRegister
+@end
---
+++ Example/Pods/Target Support Files/Pods-LoginAndRegister_Tests/Pods-LoginAndRegister_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoginAndRegister_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LoginAndRegister_Tests
+@end
